3bf8a0504bc441b9306c43f7ab613ac0e4d769a5
Fari1999 <faris.ichoutene@gmail.com>
2026-05-24 22:20:45 +0200

    harden docker deployment

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ git pull origin main
 
 docker build -t my-api .
 
+# alte Container sicher stoppen + löschen
 docker stop my-api || true
 docker rm my-api || true
 
+# neuer Container starten
 docker run -d -p 8000:8000 --name my-api my-api
-
